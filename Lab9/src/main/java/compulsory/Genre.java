dc475af2b278170e439bc6c0439c5a01e93ea85a
SmarandaS <95744493+SmarandaS@users.noreply.github.com>
2023-05-03 10:14:14 +0300

    Update Genre.java

--- Lab9/src/main/java/compulsory/Genre.java
+++ Lab9/src/main/java/compulsory/Genre.java
@@ -34,4 +34,9 @@ public class Genre {
     public void setName(String name) {
         this.name = name;
     }
+    
+    @Override
+    public String toString() {
+        return "Genre name: " + name;
+    }
 }
