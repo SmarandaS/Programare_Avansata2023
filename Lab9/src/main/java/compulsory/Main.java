85199bdf60b4278f12b62c40c61e42ed5a14da7a
SmarandaS <95744493+SmarandaS@users.noreply.github.com>
2023-05-03 10:17:51 +0300

    Update Main.java

--- Lab9/src/main/java/compulsory/Main.java
+++ Lab9/src/main/java/compulsory/Main.java
@@ -1,7 +1,26 @@
 package compulsory;
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = PersistenceUtil.getEntityManagerFactory();
+        ArtistRepository artistRepository = new ArtistRepository();
+
+        Artist artist1 = new Artist("Michael Jackson");
+        Artist artist2 = new Artist("Queen");
+        Artist artist3 = new Artist("Pink Floyd");
+        artistRepository.create(artist1);
+        artistRepository.create(artist2);
+        artistRepository.create(artist3);
+
+        List<Artist> artists = artistRepository.findByName("Queen");
+        System.out.println(artists);
+        System.out.println(artistRepository.findById(2));
+
     }
 }
