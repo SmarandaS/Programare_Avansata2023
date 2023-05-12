bcc3206e8a04099764e72c68e576ecf36b5aa91a
SmarandaS <95744493+SmarandaS@users.noreply.github.com>
2023-05-12 08:58:24 +0300

    Update PersistenceUtil.java

--- Lab9/src/main/homework/PersistenceUtil.java
+++ Lab9/src/main/homework/PersistenceUtil.java
@@ -12,13 +12,15 @@ public class PersistenceUtil {
 
     }
 
+    //daca nu exista persistence unit ul, il creeaza, altfel il returneaza
     public static EntityManagerFactory getEntityManagerFactory() {
         if (emf == null) {
             emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
         return emf;
     }
-
+    
+//inchide Entity manager factory-ul
     public static  void closeEntityManagerFactory() {
         if (emf != null && emf.isOpen()) {
             emf.close();
